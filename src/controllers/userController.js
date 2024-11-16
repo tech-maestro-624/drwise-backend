8827ffa65da3dcf2ec391ba8f7f088e22c588d14
CHANDRU <CHANDRU@Apples-MacBook-Pro.local>
2024-11-16 20:08:22 +0530

    updated on 16-11

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,7 @@ const userService = require('../services/userService')
 
 exports.get = async(req,res) => {
     try {
-        const data = await userService.get(req.body)
+        const data = await userService.get(req.query)
         return res.status(200).json(data)
     } catch (error) {
         return res.status(400).json({error : error.message})
